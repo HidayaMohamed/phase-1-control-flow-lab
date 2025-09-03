b14c478d22ad42d1bb1d260fda3c9ec25474bb49
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-09-03 08:30:16 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride) {
   // Write your code here!
+  if (ride <= 400) {
+    return 'This one is on me!'
+  } else if (ride <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if (ride <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else (ride >= 2501)
+    return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destinationCity) {
+  //Write your code here!
+  return destinationCity === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default :
+      return 'Bye.'
+  }
 }
